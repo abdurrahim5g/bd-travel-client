c17b31fb660543723c4bb2f98a38b99cc3a40060
f-rahim <freelancerrahim26@gmail.com>
2023-06-01 18:18:11 +0600

    BD Travel initial commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgQ5OqTe4CKX6N6rd2TX-ywmeRLTQkcqo",
+  authDomain: "bd-travel-5g.firebaseapp.com",
+  projectId: "bd-travel-5g",
+  storageBucket: "bd-travel-5g.appspot.com",
+  messagingSenderId: "418243176973",
+  appId: "1:418243176973:web:d68b65cc61ad5412dd0344",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
